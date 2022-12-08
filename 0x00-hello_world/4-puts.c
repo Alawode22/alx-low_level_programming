27c9f645d883dad35630076f6a6ab3f112c60c99
Alawode22 <attaurrahmanalawode@gmail.com>
2022-12-08 13:29:53 -0800

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * return; Always 0
+ */
+int main(void)
+{
+	/*string declaration*/
+	char hello[] = "\"Programming is like building a multilingual puzzle";
+	/*printing to stdout*/
+	puts(hello);
+	return (0);
+}
