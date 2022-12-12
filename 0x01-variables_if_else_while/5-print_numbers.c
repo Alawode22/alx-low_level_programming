4510e22c34317505316140cb058fb3ad770fcbe4
Alawode22 <attaurrahmanalawode@gmail.com>
2022-12-12 06:59:03 -0800

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print all digit in base 10
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	 int a;
+
+	for (a = 0; a <= 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
