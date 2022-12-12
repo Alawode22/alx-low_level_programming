8747c5edc2d26843633f9f41adfe91205ebb2dcb
Alawode22 <attaurrahmanalawode@gmail.com>
2022-12-12 07:31:53 -0800

    printingbase 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit of base 10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int g;
+
+	g = '0';
+
+	while (g < 10)
+	{
+		putchar(g + '0');
+		g++;
+	}
+	putchar('\n');
+	return (0);
+}
