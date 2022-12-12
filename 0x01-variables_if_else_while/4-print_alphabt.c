fe2f98b70f4fbf0e4dfcdd2071f7b4e07bd48c70
Alawode22 <attaurrahmanalawode@gmail.com>
2022-12-12 06:32:51 -0800

    all except qe

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Print all alpabets except q and e
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char f;
+
+	f = 'a';
+
+	while (f <= 'z')
+	{
+		if (f != 'q' && f != 'e')
+		putchar(f);
+	}
+	putchar('\n');
+	return (0);
+}
