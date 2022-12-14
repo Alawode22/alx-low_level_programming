b3dac59b641901d7168bd3219db4a125787dfcf9
Alawode22 <attaurrahmanalawode@gmail.com>
2022-12-14 02:02:14 -0800

    printing alphabet using nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - Entry
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
