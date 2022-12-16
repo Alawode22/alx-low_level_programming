79d8d42874c6f9f6ba46f1aea7e8a9bc596a16f8
Alawode22 <attaurrahmanalawode@gmail.com>
2022-12-16 03:15:20 -0800

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_line - print a straight line
+ *@n: int to be checked
+ */
+
+void print_line(int n)
+{
+	g = __________;
+
+	if (n <= '0')
+		_putchar(g);
+	else
+		_putchar('\n');
+	_putchar('\n');
+}
