38843addf1a56769f1830288b9ddeb554f1d4017
Alawode22 <attaurrahmanalawode@gmail.com>
2022-12-16 02:34:34 -0800

    task  4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,14 @@
 
 void print_most_numbers(void)
 {
-	int c;
+	char c;
 
 	for (c = '0'; c <= '9'; c++)
-		while (c != '2' && c != '4')
+	{
+		while (!(c == '2' || c == '4'))
 		{
 			_putchar(c);
 		}
-	_putchar('\n')
+	}
+		_putchar('\n')
 }
