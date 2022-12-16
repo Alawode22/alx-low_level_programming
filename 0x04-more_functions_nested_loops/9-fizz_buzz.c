833ad53c8cc494370e82c6a4ccf37f1f0a18d4d6
Alawode22 <attaurrahmanalawode@gmail.com>
2022-12-16 08:07:29 -0800

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,16 +13,14 @@ int main(void)
 	for (n = 0; n <= 100; n++)
 	{
 		if (n % 3 == 0)
-			printf(Fizz);
+			printf("Fizz");
 		else if (n % 5 == 0)
-			printf(Buzz);
+			printf("Buzz");
 		else if ((n % 5 == 0) && (n % 3 == 0))
-			printf(FizzBuzz);
+			printf("FizzBuzz");
 		else
-			printf("%n" n);
-		if (n < 100)
-			printf(' ');
+			putchar(n);
 	}
-	printf('\n');
+	putchar('\n');
 	return (0);
 }
