ebdc215b1a1e9e56344898f796928ff5630d3f51
Alawode22 <attaurrahmanalawode@gmail.com>
2022-12-20 13:18:40 -0800

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: str to be reversed
+ *Return: 0
+*/
+
+void rev_string(char *s)
+{
+	int a, b;
+
+	char c;
+
+	b = 0;
+	while (s[b] != '\0')
+		b++;
+	for (a = b - 1; a >= 0; a--)
+	{
+		c = s[a];
+	}
+}
