b36da3c0cd9ee24d35c3c680698ad2841902a308
Alawode22 <attaurrahmanalawode@gmail.com>
2022-12-20 13:47:30 -0800

    t push

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - print the other char of a str
+ * @str: char to be printed
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int a, b;
+
+	str[a] = b + 2;
+	for (b = 0; b != '\0'; b++)
+		a++
+}
