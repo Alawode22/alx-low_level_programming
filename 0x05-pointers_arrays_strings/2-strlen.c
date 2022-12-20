f7877f4d9703063cf8205164d6cc60249521f727
Alawode22 <attaurrahmanalawode@gmail.com>
2022-12-20 03:28:16 -0800

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - print the length of a string
+ * @s: char whose lenght is to be printed
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	a = sizeof(s);
+	return (a);
+}
