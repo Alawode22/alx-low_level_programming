63e69fc9e44e75283808697533dea1b5add0225b
Alawode22 <attaurrahmanalawode@gmail.com>
2022-12-20 20:22:29 -0800

    task8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - print int in in an array
+ *@a: array of int
+ *@n: num of int to be printed
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+		if (x != n - 1)
+			printf("%d, ", a[x]);
+		else
+			printf("%d", a[x]);
+	printf('\n');
+}
