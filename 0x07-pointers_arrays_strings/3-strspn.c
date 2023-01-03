cbba58adfb9d280d04c29140894adc548895ab70
Alawode22 <attaurrahmanalawode@gmail.com>
2023-01-03 09:22:45 -0800

    task2

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/** _strspn - get the length of a prefix substring
+ * @s: input
+ * @accept: input
+ * Return: 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	int r;
+
+	while (*s)
+	{
+	}
+}
