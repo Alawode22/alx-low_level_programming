e813b5804c9abc5c56bb1ca3d2235d0f214a8fdf
Alawode22 <attaurrahmanalawode@gmail.com>
2023-01-05 21:06:05 -0800

    task6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * evaluate_num - recursion loop
+ * @n: input
+ * @iterator: input
+ * Return: 0 or 1
+ */
+
+int evaluate_num(int n, int iterator)
+{
+	if (iterator == n - 1)
+		return (1);
+	else if (n % iterator == 0)
+		return (0);
+	if (n % iterator != 0)
+		return (evaluate_num(n, iterator + 1));
+	return (0);
+}
+/**
+ * is_prime_number- prints prime num
+ * @n: input
+ * Return: 0
+ */
+
+int is_prime_number(int n)
+{
+	int iterator;
+
+	iterator = 2;
+
+	if (n < 2)
+		return (0);
+	else if (n == 2)
+		return (1);
+	return (evaluate_num(n, iterator));
+}
